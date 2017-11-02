4358dfbf9d28c193b77d6c2685e0cbf81f034b39
root <root@raspberrypi.(none)>
2017-11-03 01:39:54 +0900

    Add Xml Parser Class

---
+++ backup/Assets/02.Script/UIMgr.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMgr : MonoBehaviour {
+
+    public xmlParser m_xmlLib;
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Callback
+    public void OnClickStartBtn()
+    {
+        Debug.Log("Oh~~ Yah~~~!!!");
+        if (m_xmlLib == null)
+        {
+            Debug.Log("xmlLib is Null!");
+            return;
+        }
+        m_xmlLib.exe_LoadXml("levelClass");
+    }
+}
--- backup/Assets/02.Script/xmlParser.cs
+++ backup/Assets/02.Script/xmlParser.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
 using UnityEngine;
 
-public class xmlParser : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+public class xmlParser : MonoBehaviour {
+	void Start ()
+    {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+	    	
 	}
+
+    public void exe_LoadXml(string filename)
+    {
+        TextAsset txtAsset = (TextAsset)Resources.Load("xml/" + filename);
+        XmlDocument xmlDoc = new XmlDocument();
+
+        if (xmlDoc == null)
+            return;
+
+        Debug.Log(txtAsset.text);
+        xmlDoc.LoadXml(txtAsset.text);
+    }
 }
